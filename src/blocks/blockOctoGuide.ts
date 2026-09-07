aeb56f8c8e43c07b24fdfafed2e36615f0e9db47
michael faith <michaelfaith@users.noreply.github.com>
2026-09-07 13:35:16 -0500

    add issue comment

--- src/blocks/blockOctoGuide.ts
+++ src/blocks/blockOctoGuide.ts
@@ -80,6 +80,7 @@ export const blockOctoGuide = base.createBlock({
                   with: {
                     config: addons.config ?? "recommended",
                     "github-token": "${{ secrets.GITHUB_TOKEN }}",
+                    // https://github.com/octoguide/bot/issues/624
                     rules: `{
   "pr-branch-non-default": false
 }`,
