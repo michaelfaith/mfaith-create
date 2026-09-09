cbe1fd4c196fc38f330600ca8158a0f7c8daa484
michael faith <michaelfaith@users.noreply.github.com>
2026-09-09 12:50:52 -0500

    add test coverage

--- src/blocks/blockBin.test.ts
+++ src/blocks/blockBin.test.ts
@@ -1,4 +1,4 @@
-import { testBlock } from "bingo-stratum-testers";
+import { testBlock, testIntake } from "bingo-stratum-testers";
 import { describe, expect, it } from "vitest";
 
 import { blockBin } from "./blockBin.js";
@@ -175,5 +175,71 @@ describe(blockBin, () => {
         ],
       }
     `);
+  });
+
+  describe("intake", () => {
+    it("returns undefined when package.json does not exist", () => {
+      const actual = testIntake(blockBin, {
+        files: {},
+      });
+
+      expect(actual).toBeUndefined();
+    });
+
+    it("returns undefined when package.json does not contain a bin", () => {
+      const actual = testIntake(blockBin, {
+        files: {
+          "package.json": [JSON.stringify({ name: "some-repo" })],
+        },
+      });
+
+      expect(actual).toBeUndefined();
+    });
+
+    it("returns bin when package.json contains a string bin", () => {
+      const packageJson = { bin: "dist/cli.js" };
+
+      const actual = testIntake(blockBin, {
+        files: {
+          "package.json": [JSON.stringify(packageJson)],
+        },
+      });
+
+      expect(actual).toEqual({ src: "src/cli.ts" });
+    });
+
+    it("returns bin when package.json contains a string bin with `mjs` extension", () => {
+      const packageJson = { bin: "dist/cli.mjs" };
+
+      const actual = testIntake(blockBin, {
+        files: {
+          "package.json": [JSON.stringify(packageJson)],
+        },
+      });
+
+      expect(actual).toEqual({ src: "src/cli.mts" });
+    });
+
+    it("returns bin when package.json contains an object bin", () => {
+      const packageJson = {
+        bin: {
+          "test-repo": "dist/cli.mjs",
+          "other-bin": "dist/other.cjs",
+        },
+      };
+
+      const actual = testIntake(blockBin, {
+        files: {
+          "package.json": [JSON.stringify(packageJson)],
+        },
+      });
+
+      expect(actual).toEqual({
+        src: {
+          "test-repo": "src/cli.mts",
+          "other-bin": "src/other.cts",
+        },
+      });
+    });
   });
 });
