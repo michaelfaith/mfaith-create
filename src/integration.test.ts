5d6bef30305a8cc91e1acf84985d540a335d5dcc
michael faith <michaelfaith@users.noreply.github.com>
2026-09-12 14:44:57 -0500

    test: remove redundant integration test override (#162)
    <!-- 👋 Hi, thanks for sending a PR to mfaith-create! ✨
    Please fill out all fields below and make sure each item is true and [x]
    checked.
    Otherwise we may not be able to review your PR. -->
    
    ## PR Checklist
    
    - [x] Addresses an existing open issue: fixes #158
    - [x] That issue was marked as [`status: accepting
    prs`](https://github.com/michaelfaith/mfaith-create/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [x] Steps in
    [CONTRIBUTING.md](https://github.com/michaelfaith/mfaith-create/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    <!-- Description of what is changed and how the code change does that.
    -->

--- src/integration.test.ts
+++ src/integration.test.ts
@@ -15,7 +15,6 @@ import {
   blockESLint,
   blockKnip,
   blockPnpmWorkspace,
-  blockPrettier,
   blockTemplatedWith,
   presets,
 } from "./index.ts";
@@ -136,8 +135,6 @@ If you're interested in learning more, see the 'getting started' docs on:
             },
           },
         }),
-        // TODO: This is only needed until we have a ReleasePlease block
-        blockPrettier({ ignores: ["/CHANGELOG.md"] }),
       ],
       blocks: {
         add: [blockAreTheTypesWrong],
