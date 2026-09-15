b722cbba2650081043c0bb141348ac804743450e
michael faith <michaelfaith@users.noreply.github.com>
2026-09-15 17:35:25 -0500

    fix!: use named exports
    This change shifts the index file to use named imports, and removes redundant exports `blocks` and `presets`, which are non-tree-shakable objects of all blocks and all presets, when the individual ones are already being exported.

--- src/presets/index.ts
+++ src/presets/index.ts
@@ -1,11 +1,3 @@
-import { presetCommon } from './common.ts';
-import { presetEverything } from './everything.ts';
-import { presetMinimal } from './minimal.ts';
-
-export const presets = {
-  common: presetCommon,
-  everything: presetEverything,
-  minimal: presetMinimal,
-};
-
-export { presetCommon, presetEverything, presetMinimal };
+export { presetCommon } from './common.ts';
+export { presetEverything } from './everything.ts';
+export { presetMinimal } from './minimal.ts';
