f1da157ced8bf6ca8ef6a0efc15df58682e44196
michael faith <michaelfaith@users.noreply.github.com>
2026-09-21 04:32:10 -0500

    only export `Config`

--- src/Config.ts
+++ src/Config.ts
@@ -4,8 +4,3 @@ import type { StratumRefinements } from 'bingo-stratum';
 import type { Options, OptionsShape } from './Options.ts';
 
 export type Config = TemplateConfig<OptionsShape, StratumRefinements<Options>>;
-
-export interface UserConfig {
-  options?: Partial<Options> | undefined;
-  refinements?: StratumRefinements<Options> | undefined;
-}
--- src/index.ts
+++ src/index.ts
@@ -58,7 +58,7 @@ export {
   blockVSCode,
   blockWebExt,
 } from './blocks/index.ts';
-export type { Config, UserConfig } from './Config.ts';
+export type { Config } from './Config.ts';
 export {
   presetCommon,
   presetEverything,
