bd69ccaabdb67738c5f5d432ebca0b7a09992e20
michael faith <michaelfaith@users.noreply.github.com>
2026-09-23 16:40:56 -0500

    feat: add ability for the `exports` block to define `publishConfig.exports` (#259)
    <!-- 👋 Hi, thanks for contributing to @mfaith/create! ✨
    Please fill out all fields below and make sure each item is true and [x]
    checked.
    Otherwise we may not be able to review your PR. -->
    
    ## PR Checklist
    
    - [ ] Addresses an existing open issue: fixes #000
    - [ ] That issue was marked as [`status: accepting
    prs`](https://github.com/michaelfaith/mfaith-create/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [ ] Steps in
    [CONTRIBUTING.md](https://github.com/michaelfaith/mfaith-create/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    When a `srcFilePath` is provided to the Exports block, it will use the
    `filePath` as the `publishConfig.exports` entry and the `srcFilePath`
    for the `exports` entry.

--- src/blocks/blockExports.test.ts
+++ src/blocks/blockExports.test.ts
@@ -35,11 +35,11 @@ describe(blockExports, () => {
     `);
   });
 
-  it('with addons', () => {
+  it('with addons (no leading ./', () => {
     const creation = testBlock(blockExports, {
       addons: {
         filePath: 'other.js',
-        runArgs: ['--version'],
+        srcFilePath: 'other.ts',
       },
       options: optionsBase,
     });
@@ -51,7 +51,7 @@ describe(blockExports, () => {
             "addons": {
               "properties": {
                 "exports": {
-                  ".": "./other.js",
+                  ".": "./other.ts",
                   "./package.json": "./package.json",
                 },
               },
@@ -60,8 +60,17 @@ describe(blockExports, () => {
           },
           {
             "addons": {
+              "exports": {
+                ".": "./other.js",
+                "./package.json": "./package.json",
+              },
+            },
+            "block": "[Block Publish Config]",
+          },
+          {
+            "addons": {
               "runInCI": [
-                "node other.js --version",
+                "node other.js",
               ],
             },
             "block": "[Block tsdown]",
@@ -70,4 +79,49 @@ describe(blockExports, () => {
       }
     `);
   });
+});
+
+it('with addons (with leading ./', () => {
+  const creation = testBlock(blockExports, {
+    addons: {
+      filePath: './other.js',
+      srcFilePath: './other.ts',
+    },
+    options: optionsBase,
+  });
+
+  expect(creation).toMatchInlineSnapshot(`
+    {
+      "addons": [
+        {
+          "addons": {
+            "properties": {
+              "exports": {
+                ".": "./other.ts",
+                "./package.json": "./package.json",
+              },
+            },
+          },
+          "block": "[Block Package JSON]",
+        },
+        {
+          "addons": {
+            "exports": {
+              ".": "./other.js",
+              "./package.json": "./package.json",
+            },
+          },
+          "block": "[Block Publish Config]",
+        },
+        {
+          "addons": {
+            "runInCI": [
+              "node ./other.js",
+            ],
+          },
+          "block": "[Block tsdown]",
+        },
+      ],
+    }
+  `);
 });
